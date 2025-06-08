04670d44b1a018930988331ed5e8e069b600e59e
Danny Chung <dannychung6158@gmail.com>
2025-06-07 23:16:23 -0700

    initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,30 @@
+import Die from "./Die"
+import { useState } from "react"
+
+export default function App() {
+    const [dice, setDice] = useState(generateAllNewDice());
+
+    function generateAllNewDice() {
+        const newDice = [];
+    
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.floor((Math.random() * 6) + 1));
+        }
+    
+        return newDice
+    }
+
+    const randomDice = dice.map( number => {
+        return <Die value={number} />
+    })
+
+    return (
+        <main>
+            <div className="game">
+                <div className="dice">
+                    {randomDice}
+                </div>
+            </div>
+        </main>
+    )
+}
