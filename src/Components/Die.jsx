c4625b523fc619b7973dcfc08b73b2803a33e1f0
Danny Chung <dannychung6158@gmail.com>
2025-06-14 17:25:01 -0700

    Conditionally rendered the classname based on isHeld prop

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,6 +1,7 @@
 export default function Die(props) {
+    console.log(props);
     return (
-        <button className="die">
+        <button className={"die " + (props.isHeld ? "held" : "")}>
             {props.value}
         </button>
     )
