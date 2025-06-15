0b606122eba9f6002a7090d9854fa7f91c867ea4
Danny Chung <dannychung6158@gmail.com>
2025-06-14 20:02:56 -0700

    Added conditional aria attributes to the dice

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -3,6 +3,8 @@ export default function Die(props) {
         <button 
             className={"die " + (props.isHeld ? "held" : "")}
             onClick={props.holdFunction}
+            aria-pressed={props.isHeld}
+            aria-label={`Die is ${props.isHeld ? "held" : "not held"} with a value of ${props.value}`}
         >
             {props.value}
         </button>
